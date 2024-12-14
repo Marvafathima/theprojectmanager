e0f02979d940123ef1eba5093bc505622c254fd3
Marvafathima <marvafathima62@gmail.com>
2024-12-14 23:40:47 +0530

    footer changed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export function FooterWithLogo() {
   return (
     <footer className="w-full bg-deep_orange-700 p-8">
       <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-deep_orange-700 text-center md:justify-between">
-        <img src="orangen.png" alt="Narrato" className="w-10" />
+        <img src="orangen.png" alt="TheProjectManager" className="w-10" />
         <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
           <li>
             <Typography
@@ -50,7 +50,7 @@ export function FooterWithLogo() {
       </div>
       <hr className="my-8 border-white" />
       <Typography color="white" className="text-center font-normal">
-        &copy; 2023 Narrato
+        &copy; 2023 TheProjectManager
       </Typography>
     </footer>
   );
